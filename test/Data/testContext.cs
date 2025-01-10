90d828d6d95f33944b69e5b51801c83334ab7965
Jake <essigj88@outlook.com>
2025-01-09 21:21:35 -0500

    Add project files.

---
+++ test/Data/testContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using test.Models;
+
+namespace test.Data
+{
+    public class testContext : DbContext
+    {
+        public testContext (DbContextOptions<testContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Student> Students { get; set; } = default!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Course>().ToTable("Course");
+            modelBuilder.Entity<Enrollment>().ToTable("Enrollment");
+            modelBuilder.Entity<Student>().ToTable("Student");
+        }
+    }
+}
---
+++ test/Program.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using test.Data;
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<testContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("testContext") ?? throw new InvalidOperationException("Connection string 'testContext' not found.")));
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<testContext>();
+    DbInitializer.Initialize(context);
+}
+app.Run();
